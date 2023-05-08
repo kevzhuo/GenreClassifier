36d48da4e58bd7331969df5b5abcb9ca87ee227d
kevzhuo <kzhuo2014@gmail.com>
2023-05-08 16:45:12 -0400

    changes

--- multimodal.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import torch as nn
-import pandas as p
-
-# Multimodal Approach
---
+++ preprocess.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch as nn
+import pandas as pd
+import transformers
+from transformers import BertTokenizer, BertForSequenceClassification, TrainingArguments, Trainer, DataCollatorWithPadding
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+
+#Importing the data and cleaning it
+df = pd.read_csv('./spotify_songs.csv')
+df = df.drop(df[df["language"]!="en"].index)
+songs = df[['lyrics','playlist_genre']]
+songs = songs.dropna()
+songs = songs.reset_index(drop=True)
+num_genres = songs['playlist_genre'].nunique()
+
+#Converts the musical genre strings into numerical values
+le = LabelEncoder()
+le.fit(df["playlist_genre"])
+songs["genre_num"] = le.transform(songs["playlist_genre"])
+
+num_genres = songs['playlist_genre'].nunique()
+
+#Splitting the dataset into train, validation, test
+songs_train, songs_val, train_label, val_label = train_test_split(songs['lyrics'], songs['genre_num'], test_size=0.3, random_state=5)
+songs_val, songs_test, val_label, test_label = train_test_split(songs_val, val_label, test_size=0.5, random_state=5)
+
+tokenizer = transformers.BertTokenizer.from_pretrained('distilbert-base-uncased')
+model = transformers.BertForSequenceClassification.from_pretrained('distilbert-base-uncased')
+def preprocess_func(data):
+    return tokenizer.batch_encode_plus(data, truncation=True, padding = True, return_tensors="pt")
+
+#Tokenizing the lyrics
+lyrics_train = preprocess_func(songs_train.tolist())
+lyrics_val = preprocess_func(songs_val.tolist())
+lyrics_test = preprocess_func(songs_test.tolist())
+
+#lyrics_train["label"] = train_label
+#lyrics_val["label"] = val_label
+#lyrics_test["label"] = nn.tensor(test_label.tolist())
+
